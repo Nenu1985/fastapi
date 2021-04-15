e1b202f841b7cf1337cf128d1e280980ce06c89a
Nenu1985 <nenuzhny85@gmail.com>
2021-04-15 23:43:30 +0300

    b

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from schemas import Item
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Path, Query
 
 app = FastAPI()
 
@@ -8,10 +9,34 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/items/")
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+    return item
+
+@app.post("/items_/")
 async def create_item(item: Item):
     item_dict = item.dict()
     if item.tax:
         price_with_tax = item.price + item.tax
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
+
+# declare path parameters and request body at the same time
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+
+@app.post("/login/")
+async def login(username: str = Form(...), password: str = Form(...)):
+    return {"username": username}
+
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: int = Path(..., title="The ID of the item to get"),
+    q: Optional[str] = Query(None, alias="item-query"),
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
